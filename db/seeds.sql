21525cc08aebaf1c8ea0c4f2c3335bef57bd65f7
patorrad <paolo.a.torrado@gmail.com>
2020-01-17 21:08:29 -0800

    Full Mods

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Messy Cheesy');
+INSERT INTO burger (name) VALUES ('Fat burger');
+INSERT INTO burger (name, eaten) VALUES ('Classic burger', true);
+INSERT INTO burger (name, eaten) VALUES ('Emerald burger', true);
+INSERT INTO burger (name, eaten) VALUES ('Obamabama', true);
+INSERT INTO burger (name) VALUES ('Heart Attack burger');
