c463b562b6ced9d3bf588d34e7718f9f8778e01c
patorrad <paolo.a.torrado@gmail.com>
2020-01-18 15:36:40 -0800

    Added ClearDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burger_db"
 });
+};
+
+//mysql://b633cf825fa8ea:9b9fac6e@us-cdbr-iron-east-05.cleardb.net/heroku_2e2cb7e01d6da89?reconnect=true
 
 // Make connection.
 connection.connect(function(err) {
