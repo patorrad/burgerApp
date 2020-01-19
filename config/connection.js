d2caea3d99499d4f4d931fa261e8724514a7c803
patorrad <paolo.a.torrado@gmail.com>
2020-01-19 13:25:10 -0800

    Changed connection to pool

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
+var pool;
 
 if (process.env.CLEARDB_DATABASE_URL) {
-  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+  pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 } else {
-  connection = mysql.createConnection({
+  pool = mysql.createPool({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -18,7 +18,7 @@ if (process.env.CLEARDB_DATABASE_URL) {
 //mysql://b633cf825fa8ea:9b9fac6e@us-cdbr-iron-east-05.cleardb.net/heroku_2e2cb7e01d6da89?reconnect=true
 
 // Make connection.
-connection.connect(function(err) {
+pool.getConnection(function(err, connection) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -27,4 +27,4 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = pool;
--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,5 @@ CREATE TABLE `heroku_2e2cb7e01d6da89`.`burger`
 	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+SELECT * FROM `heroku_2e2cb7e01d6da89`.`burger`
